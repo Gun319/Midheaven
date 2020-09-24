3f49090a9ae7eb3ee5470a58e61f81b29ccdaec7
fen he <2249464899@qq.com>
2020-09-24 16:09:10 +0800

    第三次提交

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -11,7 +11,7 @@ namespace Midheaven.Controllers
         // GET: Admin
         public ActionResult Login()
         {
-            //用户的
+            //用户的登录页
             return View();
         }
     }
