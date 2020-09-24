bff0030e85f5f5023fef206aa65bfe5cb36a160f
y2324169439@hotmail.com <y2324169439@hotmail.com>
2020-09-24 16:22:50 +0800

    第三次提交

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -11,7 +11,7 @@ namespace Midheaven.Controllers
         // GET: Admin
         public ActionResult Login()
         {
-            //用户的登录
+            //用户的登录大多数
             return View();
         }
     }
