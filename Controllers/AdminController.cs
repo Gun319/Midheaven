b65fa3131705b9c9bad24ebd769ff59abb56f3a1
Gun319 <493469635@qq.com>
2020-09-25 09:23:54 +0800

    新增

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -9,9 +9,57 @@ namespace Midheaven.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Login()
+
+        /// <summary>
+        /// 管理员首页
+        /// 默认显示所有成员信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AdminIndex()
         {
-            //用户的登录1124
+            return View();
+        }
+
+        /// <summary>
+        /// 查询所有课程信息
+        /// 不包含评论信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult AdminCourse()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 添加成员
+        /// 添加所有信息(成员类型),成员默认通过审核
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult AdminAddMember()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 添加课程
+        /// 需指定教师,并添加该课程所有信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AdminAddCourse()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 删除成员
+        /// 根据成员的id进行删除操作
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult DelMemberByCID()
+        {
             return View();
         }
     }
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Midheaven.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        
+        /// <summary>
+        /// 登录
+        /// 判断用类型,根据用户类型跳转至不同的页面,使用异步判断
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
