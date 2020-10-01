b9f2bcb68b0b2371ca50a88844c102408fd700db
Gun319 <493469635@qq.com>
2020-10-01 11:12:57 +0800

    11:12

---
+++ script/Login.js
@@ -0,0 +1,63 @@
+﻿$(function () {
+    Victor("container", "output");   //登录背景函数
+    $("#UserName").focus(); //默认选中
+
+    $("#Password,#UserName").keydown((event) => {
+        if (event.keyCode == "13") {
+            // 调用Ajax方法
+            if (($("#UserName").val().length != 0) & $("#Password").val().length != 0)
+                PostData();
+            // 非空提示
+            if ($("#UserName").val().length == 0)
+                $("#namePrompt").text("用户名不能为空");
+
+            if ($("#Password").val().length == 0)
+                $("#pwdPrompt").text("密码不能为空");
+        }
+    })
+});
+
+$("#entry_btn").click(() => {
+    PostData();
+});
+
+/**
+登录异步验证
+*/
+function PostData() {
+    var uName = $("#UserName").val();
+    var uPwd = $("#Password").val();
+    $.ajax({
+        url: '/Login/Login',
+        data: { username: uName, userpwd: uPwd },
+        type: 'post',
+        dataType: 'json',
+        success: (data) => {
+            if (data != 0) {
+                $.each(data, (index, item) => {
+                    if (item.M_Flog == 0) {
+                        if (item.R_ID == 1)
+                            window.location.href = "/Admin/AdminIndex";
+                        if (item.R_ID == 2)
+                            window.location.href = "/Member/MemberByTeacher";
+                        if (item.R_ID == 3)
+                            window.location.href = "/Member/MemberByStudent";
+                    } else {
+                        $("#prompt").text("账户已注销请联系管理员");
+                    }
+                });
+            } else {
+                $("#prompt").text("用户名或密码错误");
+            }
+        }
+    });
+}
+
+/**
+清除lable标签内容
+*/
+$("#UserName,#Password").focus(() => {
+    $("#prompt").text("");
+    $("#namePrompt").text("");
+    $("#pwdPrompt").text("");
+});
---
+++ script/Register.js
@@ -0,0 +1,70 @@
+﻿$(document).ready(function () {
+    var height = $(document).height();
+    $(".main").css("height", height);
+});
+
+/**
+ 发送邮箱验证码
+ */
+$("#emailCode").click(() => {
+    var Email = $("#Email").val();
+    if (Email.length != 0) {
+        $.ajax({
+            url: "/Member/sendEmail",
+            type: "post",
+            data: { direction: Email },
+            success: (data) => {
+                if (data == 200) {
+                    $("#emailCode").text("验证码已发送");
+                } else {
+                    $("#emailPrompt").text("服务器繁忙");
+                }
+            },
+        });
+    }
+    else {
+        $("#emailPrompt").text("请输入邮箱");
+    }
+});
+
+//还原按钮文字
+$("#Email").focus(() => {
+    $("#emailCode").text("获取验证码");
+    $("#emailPrompt").text("");
+});
+
+$("#uPwd1").focus(() => {
+    $("#pwdlPrompt").text("");
+});
+
+/**
+ 注册
+ */
+$("#ZC").click(() => {
+    var code = $("#EmailCode").val();
+    var role = $("#radRole:checked").val();
+    var uname = $("#uName").val();
+    var upwd = $("#uPwd").val();
+    var upwd1 = $("#uPwd1").val();
+    if (code.length != 0) {
+        if (upwd == upwd1) {
+            $.ajax({
+                url: "/Member/Register",
+                type: "post",
+                data: { newcode: code, Role: role, uName: uname, uPwd: upwd },
+                success: (data) => {
+                    if (data == 200) {
+                        $("#login").append("注册成功 <a href='/Login/Login'>去登录</a>");
+                    }
+                    if (data == 201)
+                        $("#codePrompt").text("验证码错误");
+                },
+            });
+        } else {
+            $("#pwdlPrompt").text("两次密码不一致");
+        }
+    } else {
+        $("#codePrompt").text("请填写验证吗");
+        $("#EmailCode").focus();
+    }
+});
