7351ab1a6320e290dfa20c6262f95ccd72c5141b
Gun319 <493469635@qq.com>
2020-10-14 18:01:38 +0800

    完结2

--- script/AddCouser.js
+++ script/AddCouser.js
@@ -15,17 +15,35 @@ function ShowImg(newfilename) {
 function UpCourse() {
     var name = $("#C_Name").val();
     var desc = $("#C_Desc").val();
-    $.ajax({
-        url: "/Member/AddCourse",
-        type: "post",
-        data: { cName: name, CDesc: desc, imgname: imgname },
-        success: (data) => {
-            if (data == 200) {
-                alert('上传成功');
-                window.location.href = "/Member/SelCouser";
-            } else {
-                alert('服务器繁忙');
+    if ((name.length != 0) & (desc.length != 0) & (imgname.length != 0)) {
+        $.ajax({
+            url: "/Member/AddCourse",
+            type: "post",
+            data: { cName: name, CDesc: desc, imgname: imgname },
+            success: (data) => {
+                if (data == 200) {
+                    alert('上传成功');
+                    window.location.href = "/Member/SelCouser";
+                } else {
+                    alert('服务器繁忙');
+                }
             }
-        }
-    });
+        });
+    }
+
+    if (name.length == 0)
+        $("#name").text('请填写课程名称');
+    if (desc.length == 0)
+        $("#desc").text('请填写课程简介');
+    if (imgname.length == 0)
+        $("#img").text('请上传课程图片');
 }
+
+/**
+清除lable标签内容
+*/
+$("#C_Name,#C_Desc").focus(() => {
+    $("#name").text("");
+    $("#desc").text("");
+    $("#img").text("");
+});
---
+++ script/SelStudentCourse.js
@@ -0,0 +1,46 @@
+﻿selpage();
+
+/**
+ 显示已选课程
+ */
+function selpage() {
+    $.ajax({
+        url: "/Member/SelCourse",
+        type: "post",
+        data: {},
+        success: function (data) {
+            var str = "";
+            $.each(data, function (index, item) {
+                str += "<tr>";
+                str += "<td>" + item.C_ID + "</td>";
+                str += "<td>" + item.C_Name + "</td>";
+                str += "<td>" + item.C_Desc + "</td>";
+                str += "<td><button type='button' class='btn btn-danger'id='btnup' onclick='Del(" + item.ID + ");'>退课</button></td>";
+                str += "</tr>";
+            })
+            $("#dg tbody tr").remove();
+            $("#dg tbody").append(str);
+            return;
+        }
+    });
+}
+
+/**
+ 退课
+ */
+function Del(id) {
+    $.ajax({
+        url: "/Member/DelStudentCourse",
+        type: "post",
+        data: { id: id },
+        success: function (data) {
+            if (data = 200) {
+                alert('退课完成');
+                return;
+            }
+            else {
+                alert('退课失败，请检查网络连接');
+            }
+        }
+    });
+}
